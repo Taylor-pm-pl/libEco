31611c05f044c9b489d157874c6964e7c7523dea
YTBJero <mw.sevice@gmail.com>
2022-03-15 01:09:57 +0000

    Update Economy.php

--- src/YTBJero/LibEconomy/Economy.php
+++ src/YTBJero/LibEconomy/Economy.php
@@ -16,7 +16,7 @@ final class Economy{
 	/**
 	 * @return array
 	 */
-	private function getEconomy(): array{
+	private function getEconomy(){
 		$api = PMServer::getInstance()->getPluginManager()->getPlugin("EconomyAPI");
 		if($api !== null){
 			return [self::ECONOMYAPI, $api];
